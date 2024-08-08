fed707cfd616aba611712b9fdc1feb987f456b76
Bruno Rodrigues <brunorodrigues-78@hotmail.com>
2024-08-08 10:54:45 -0300

    created login screen

--- app/src/main/java/br/com/brunorodrigues/tasklist/LoginActivity.kt
+++ app/src/main/java/br/com/brunorodrigues/tasklist/LoginActivity.kt
@@ -1,14 +1,11 @@
 package br.com.brunorodrigues.tasklist
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
