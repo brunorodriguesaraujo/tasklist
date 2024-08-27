e3f80af4fadd8fe9d1e5e4b6f2955558558d0ae6
Bruno Rodrigues <brunorodrigues-78@hotmail.com>
2024-08-27 19:21:47 -0300

    add menu toolbar

--- app/src/main/java/br/com/brunorodrigues/tasklist/home/HomeActivity.kt
+++ app/src/main/java/br/com/brunorodrigues/tasklist/home/HomeActivity.kt
@@ -3,7 +3,10 @@ package br.com.brunorodrigues.tasklist.home
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import br.com.brunorodrigues.tasklist.R
 import br.com.brunorodrigues.tasklist.databinding.ActivityHomeBinding
 import br.com.brunorodrigues.tasklist.model.TaskModel
 import br.com.brunorodrigues.tasklist.signin.LoginActivity
@@ -15,18 +18,39 @@ class HomeActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHomeBinding
     private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initFirebaseAuth()
+        setToolbar()
         initAdapter()
-        setViews()
         setListener()
     }
 
     private fun initFirebaseAuth() {
         auth = Firebase.auth
+    }
+
+    private fun setToolbar() {
+        setSupportActionBar(binding.toolbarLayout.toolbar)
+        supportActionBar?.title = getString(R.string.task_list_label)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_home, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.logout -> {
+                signOut()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     @SuppressLint("NotifyDataSetChanged")
@@ -35,19 +59,16 @@ class HomeActivity : AppCompatActivity() {
             TaskModel(
                 "1",
                 "title",
-                "alo",
                 "24 de ago de 2024"
             ),
             TaskModel(
                 "2",
                 "title2",
-                "aloo",
                 "24 de ago de 2024"
             ),
             TaskModel(
-                "2",
-                "title2",
-                "aloo",
+                "3",
+                "title3",
                 "25 de ago de 2024"
             ),
         )
@@ -58,16 +79,14 @@ class HomeActivity : AppCompatActivity() {
     }
 
     private fun prepareItems(list: List<TaskModel>): List<Any> {
-        return list.groupBy { it.date }.flatMap { (date, tasks) ->
+        return list.reversed().groupBy {
+            it.date
+        }.flatMap { (date, tasks) ->
             listOf(date) + tasks
         }
     }
 
-    private fun setViews() {
-    }
-
     private fun setListener() {
-//        binding.toolbar.setClickButtonLogout { signOut() }
     }
 
     private fun signOut() {
--- app/src/main/java/br/com/brunorodrigues/tasklist/model/TaskModel.kt
+++ app/src/main/java/br/com/brunorodrigues/tasklist/model/TaskModel.kt
@@ -3,6 +3,5 @@ package br.com.brunorodrigues.tasklist.model
 data class TaskModel(
     val id: String,
     val title: String,
-    val description: String,
     val date: String
 )
